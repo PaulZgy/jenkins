e622516cfbd3026b2c38e5d9967dbe9c4bfdf9e5
tom0949 <514199242@qq.com>
2022-07-16 23:27:47 +0800

    'init'

---
+++ src/test/java/cn/highsheep/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.highsheep.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
